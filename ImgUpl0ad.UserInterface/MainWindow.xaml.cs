15cd32d44eb703c88a7da6c3e7bb4fc0ccd3a3a9
RainbowCrashie <nexus556.45@gmail.com>
2015-10-26 23:03:42 +0900

    Mahapps.Metro Implemented

--- ImgUpl0ad.UserInterface/MainWindow.xaml.cs
+++ ImgUpl0ad.UserInterface/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ImgUpl0ad
+namespace ImgUpl0ad.UserInterface
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
