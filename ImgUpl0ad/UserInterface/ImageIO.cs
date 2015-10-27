9707c386d38d88a59f812c3020163ede8821654a
RainbowCrashie <nexus556.45@gmail.com>
2015-10-28 05:35:28 +0900

    Opening file by dialog implemented. ImageIo methods will return bool. To grab the data, it is needed to access ImageIo.SelectedImage. (OpenFileDlg style)
    Opening/Closing a file wil automatically flips the state of UploadButton.IsEnabled from MainWindow.SelectedImage's set accessor.

---
+++ ImgUpl0ad/UserInterface/ImageIO.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace ImgUpl0ad.UserInterface
+{
+    public class ImageIo : IDisposable
+    {
+        private string[] CompatibleExtentions { get; } = { ".png", ".jpg", ".jpeg", ".gif", ".bmp" };
+
+        public ImageBuff SelectedImage { get; set; }
+
+        private bool CheckExtentionCompatible(string filePath)
+        {
+            return CompatibleExtentions.Contains(Path.GetExtension(filePath), StringComparer.OrdinalIgnoreCase);
+        }
+
+        public bool OpenFileWithDialog()
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "*.jpg;*.png;*.gif;*.bmp;|*.jpg;*.png;*.gif;*.bmp;";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == false)
+                return false;
+
+            SelectedImage = new ImageBuff(new BitmapImage(new Uri(openFileDialog.FileName)), openFileDialog.FileName);
+            return true;
+        }
+
+#region "destractor"
+        private bool _disposed = false;
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~ImageIo()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+               //Dispose everyrhing
+               SelectedImage = null;
+            }
+            _disposed = true;
+        }
+#endregion
+
+    }
+
+    public class ImageBuff
+    {
+        public ImageBuff(BitmapImage imageSouce, string detail)
+        {
+            ImageSouce = imageSouce;
+            Detail = detail;
+        }
+
+        public BitmapImage ImageSouce { get; set; }
+        public string Detail { get; set; }
+    }
+}
--- ImgUpl0ad/UserInterface/MainWindow.xaml.cs
+++ ImgUpl0ad/UserInterface/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 using MahApps.Metro.Controls;
 
 namespace ImgUpl0ad.UserInterface
@@ -7,11 +8,36 @@ namespace ImgUpl0ad.UserInterface
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private ImageBuff _selectedimage;
+        private ImageBuff SelectedImage
+        {
+            get { return _selectedimage; }
+            set
+            {
+                if (_selectedimage == value) return;
+                ButtonUpload.IsEnabled = value != null;
+                _selectedimage = value;
+            }
+        }
+
+        private void OpenBydialog(object sender, ExecutedRoutedEventArgs e)
+        {
+            var opener = new ImageIo();
+            using (opener)
+            {
+                if (opener.OpenFileWithDialog() == false)
+                    return;
+
+                SelectedImage = opener.SelectedImage;
+                MainImage.Source = SelectedImage.ImageSouce;
+            }
         }
     }
 }
