94d01f3b0095ed1a242f5bea0df1f2e3600ebb51
RainbowCrashie <nexus556.45@gmail.com>
2015-11-02 01:56:26 +0900

    UploadWindow

--- ImgUpl0ad/Imgur.cs
+++ ImgUpl0ad/Imgur.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using System.Windows.Media.Imaging;
---
+++ ImgUpl0ad/UserInterfaces/UploadResultWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+using MahApps.Metro.Controls;
+
+namespace ImgUpl0ad.UserInterfaces
+{
+    /// <summary>
+    /// Interaction logic for UploadResultWindow.xaml
+    /// </summary>
+    public partial class UploadResultWindow
+    {
+        public UploadResultWindow(ImageBuff source)
+        {
+            InitializeComponent();
+
+            Upload(source);
+        }
+
+        private async void Upload(ImageBuff source)
+        {
+            
+        }
+
+        private void ButtonOk_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
