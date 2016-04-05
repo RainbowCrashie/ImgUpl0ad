68bc60b5f47aa89cf5fa95fa5fe38a8db87e54a3
Star Swirl <nexus556.45@gmail.com>
2016-04-05 12:08:11 +0900

    Upload Result Model constructed PrincessLuna: Code supplies to MVVM architecture  - ObservableObject: implementation of INotifyPropertyChanged. makes properties bindable  - RelayCommand: ICommand object

---
+++ ImgUpl0ad/History/UploadResultModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows;
+
+using PrincessLuna;
+
+namespace ImgUpl0ad.History
+{
+    public class UploadResultModel : ObservableObject
+    {
+        #region fields
+
+        private string _directLink;
+        private string _originalname;
+        private Point _resolution;
+        private long _fileSize;
+        private DateTime _uploadedDate;
+        private string _deleteHash;
+
+        #endregion
+
+        public string DirectLink
+        {
+            get { return _directLink; }
+            set
+            {
+                if (value == _directLink)
+                    return;
+
+                _directLink = value;
+                OnPropertyChanged(nameof(DirectLink));
+            }
+        }
+
+        public string OriginalName
+        {
+            get { return _originalname; }
+            set
+            {
+                if (value == _originalname)
+                    return;
+
+                _originalname = value;
+                OnPropertyChanged(nameof(OriginalName));
+            }
+        }
+
+        public Point Resolution
+        {
+            get { return _resolution; }
+            set
+            {
+                if (value == _resolution)
+                    return;
+
+                _resolution = value;
+                OnPropertyChanged(nameof(Resolution));
+            }
+        }
+
+        public long FileSize
+        {
+            get { return _fileSize; }
+            set
+            {
+                if (value == _fileSize)
+                    return;
+
+                _fileSize = value;
+                OnPropertyChanged(nameof(FileSize));
+            }
+        }
+
+        public DateTime UploadedDate
+        {
+            get { return _uploadedDate; }
+            set
+            {
+                if (value == _uploadedDate)
+                    return;
+
+                _uploadedDate = value;
+                OnPropertyChanged(nameof(UploadedDate));
+            }
+        }
+
+        public string DeleteHash
+        {
+            get { return _deleteHash; }
+            set
+            {
+                if (value == _deleteHash)
+                    return;
+
+                _deleteHash = value;
+                OnPropertyChanged(nameof(_deleteHash));
+            }
+        }
+    }
+}
